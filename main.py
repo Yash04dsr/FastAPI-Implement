b006223ebb81196c6aa36020524d3302315731dd
Yash <yashsakhare.iitd@gmail.com>
2025-06-12 01:50:16 +0530

    added the database

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI() #Create an instance of FastAPI
 
-
+"""Info:::
 #Survers se data lena chahte ho -> get request
-#Survers pe data bhejna chahte ho -> post request
+#Survers pe data bhejna chahte ho -> post request """
 
 @app.get("/") #Define a route for the root URL
 def ji():
@@ -13,3 +13,4 @@ def ji():
 @app.get("/about")
 def about():
     return{"message":"Teri Ma ki Jay"} #Define a route for the /hello URL
+
