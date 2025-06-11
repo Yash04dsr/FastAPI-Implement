464ad5ee041a0cbba0b65a70e4330b2c7dc8244c
Yash <yashsakhare.iitd@gmail.com>
2025-06-12 02:30:33 +0530

    New endpoint of View

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+import json
+
+#Reading the JSON file
+def load_data():
+    with open("patients.json", "r") as f:
+        data = json.load(f)
+    return data
+
 
 app = FastAPI() #Create an instance of FastAPI
 
@@ -8,9 +16,21 @@ app = FastAPI() #Create an instance of FastAPI
 
 @app.get("/") #Define a route for the root URL
 def ji():
-    return {"message": "Hello, World!"}
+    return {"message": "A patient management system for doctors"} #Return a welcome message
 
 @app.get("/about")
 def about():
-    return{"message":"Teri Ma ki Jay"} #Define a route for the /hello URL
+    return{"message":"A fully functional api to manage patients records"} #Define a route for the /hello URL
+
+@app.get("/view")
+def view():
+    data=load_data()
+    return data
+
+    
+    
+
+
+
+
 
