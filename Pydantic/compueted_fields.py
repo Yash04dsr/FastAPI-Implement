28c515167712e024d7e2dc483086e27f217a9f79
Yash <yashsakhare.iitd@gmail.com>
2025-06-13 05:34:52 +0530

    computed_fiel ds

---
+++ Pydantic/compueted_fields.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel, EmailStr, computed_field
+from typing import List, Dict
+
+class Patient(BaseModel):
+
+    name: str
+    email: EmailStr
+    age: int
+    weight: float # kg
+    height: float # mtr
+    married: bool
+    allergies: List[str]
+    contact_details: Dict[str, str]
+
+    #Computed field help us to create new fields dynamically using the field already exists
+
+    @computed_field
+    @property #telling that this is also a new filed created via us, so that i can acces the same after this point as wel
+    
+
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2),2)
+        return bmi
+
+
+
+def update_patient_data(patient: Patient):
+
+    print(patient.name)
+    print(patient.age)
+    print(patient.allergies)
+    print(patient.married)
+    print('BMI', patient.bmi)
+    print('updated')
+
+patient_info = {'name':'nitish', 'email':'abc@icici.com', 'age': '65', 'weight': 75.2, 'height': 1.72, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details':{'phone':'2353462', 'emergency':'235236'}}
+
+patient1 = Patient(**patient_info) 
+
+update_patient_data(patient1)
